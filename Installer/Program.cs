fca6bd10e04d399e3d4a8e049635bef722e5b5d2
Felipe Oliveira <felipe.oliveira@vortigo.com.br>
2020-09-21 05:20:34 -0300

    First Commit 🚀🚀

---
+++ Installer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Installer
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            // The TopShelf and Squirrel stuff goes here
+        }
+    }
+}
---
+++ Runner/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Runner
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Simple console application to run some manual tests
+        }
+    }
+}
