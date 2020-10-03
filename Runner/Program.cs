0ecc73fee03a7a60161c4552d4b5a81419ebefa7
Felipe Oliveira <felipexk1@live.com>
2020-10-02 22:50:37 -0300

    ♻ Improve Readme

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -7,18 +7,16 @@ namespace Runner
     internal static class Program
     {
         private static SecurityManager _manager;
-
         public static void Main()
         {
-            Init();
-        }
-
-        private static void Init()
-        {
-            var processWatcher = new LeagueProcessWatcher();
+            // you must provide an implementation of `TextFileWatcher<Lockfile>`
             var lockfileWatcher = new LockfileWatcher(new LockFileHandler());
+            var processWatcher = new LeagueProcessWatcher();
 
+            // references may be provided by some IOC container
             _manager = new SecurityManager(processWatcher, lockfileWatcher);
+            
+            // this call will block the main Thread, this whole manager should run within dedicated Thread. 
             _manager.Start();
         }
     }
